29b381e5908bb5e5c030cef7250fc559f30a7f21
JasonRodriguezGarcia <jasonrodriguezempleo.gmail.com>
2025-02-26 13:15:32 +0100

    cambios en chatterminator+Portal

--- 20250219/js/18chatgpt.js
+++ 20250219/js/18chatgpt.js
@@ -9,15 +9,45 @@ function enviarMensaje() {
 //         <p>${}
 //     `
     inputTexto = document.querySelector("#texto")
-    nuevoParrafo = document.createElement("p")
-    nuevoParrafo.innerHTML = '<sup class="sup">*Yo*</sup>' + inputTexto.value
-    nuevoParrafo.classList.add("textoInput")
-    document.querySelector("main").appendChild(nuevoParrafo)
-    inputTexto.value = ""
-    main.scrollTo(0, main.scrollHeight)
-    texto.focus()
+
+    if (inputTexto.value != '') {
+        nuevoParrafo = document.createElement("p")
+        nuevoParrafo.innerHTML = '<sup class="sup">*Yo*</sup>' + inputTexto.value
+        nuevoParrafo.classList.add("textoInput", "textoInputAnchura")
+        document.querySelector("main").appendChild(nuevoParrafo)
+        inputTexto.value = ""
+        main.scrollTo(0, main.scrollHeight)
+        texto.focus()
+    }
 }
 
-function addImage(e) {
+function addImage() {
     
+    inputFichero = document.querySelector("#fichero")
+    file = inputFichero.files[0]
+    const reader = new FileReader();
+
+    if (file) {
+        reader.readAsDataURL(file)
+
+        reader.onload = function (e) {
+            // sitodo va bien
+            console.log("todo bien!")
+            // div.innerHTML = e.target.result
+            // div.textContent = e.target.result
+            imagen = document.createElement("img")
+            imagen.src = e.target.result
+            imagen.classList.add("textoInput", "ficheroMargenes")
+            main.appendChild(imagen)
+        }
+        reader.onloadend = function (e) {
+            file = ''
+            main.scrollTo(0, main.scrollHeight)
+            texto.focus()
+        }
+        reader.onerror = function(e) {
+            console.log("error")
+        }
+    }
+
 }
