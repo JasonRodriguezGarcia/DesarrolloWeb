73e1c845aec89d25f4d70094d4e9f76556010630
JasonRodriguezGarcia <jasonrodriguezempleo.gmail.com>
2025-03-02 21:58:00 +0100

    añadiendo en cada directorio, descripción de los ejercicios realizados cada día

--- 20250228/a1.js
+++ 20250228/a1.js
@@ -4,8 +4,9 @@
 // estando este fichero js al principio, dará un error porque
 // se usa antes de cargar, se usa el defer en la llamada al este fichero
 // para evitarlo
-// document.getElementById("titulo").innerHTML = "CAMbio"
+document.getElementById("titulo").innerHTML = "CAMbio"
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('titulo').innerHTML="CamBIO"
-})
+// otra forma de hacer el cambio con eventlistener
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('titulo').innerHTML="CamBIO"
+// })
